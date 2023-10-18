a15727d158730a20c1b136fbf614d4f27fdf1cce
Christopher Endress <95492688+cendress@users.noreply.github.com>
2023-10-18 09:23:07 -0400

    Add project1

---
+++ StormViewer/PictureCell.swift
@@ -0,0 +1,23 @@
+//
+//  PictureCell.swift
+//  StormViewer
+//
+//  Created by Christopher Endress on 10/18/23.
+//
+
+import UIKit
+
+class PictureCell: UITableViewCell {
+  @IBOutlet weak var cellTitleLabel: UILabel!
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+  }
+  
+}
